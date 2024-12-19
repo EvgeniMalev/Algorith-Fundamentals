8973fe8f2f97b3dee7863110c53255ec0c37afbb
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-12-19 23:09:36 +0200

    Create shufflemethod.java

---
+++ shufflemethod.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ShuffleExample {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            numbers.add(i);
+        }
+
+        System.out.println("Original List: " + numbers);
+
+        Collections.shuffle(numbers);
+
+        System.out.println("Shuffled List: " + numbers);
+    }
+}
